4f99cb179c9421e3ef7fa500f4b3718259c45261
Jeffrey Patton <jeffrey@patton-tech.com>
2020-09-08 11:17:03 -0500

    Added process block

--- schema/schema.psm1
+++ schema/schema.psm1
@@ -41,25 +41,28 @@ function Get-Object {
     [Parameter(ValueFromPipeline)]
     [object]$Schema
   )
-  $Properties = $Schema.properties | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name;
-  $Members = @{};
-  foreach ($Property in $Properties) {
-    switch ($Schema.properties.$Property.type) {
-      'object' {
-        $Members.Add($Property, (New-Object -TypeName psobject -Property @{}))
-      }
-      'array' {
-        $Members.Add($Property, @())
-      }
-      'string' {
-        $Members.Add($Property, "")
-      }
-      'number' {
-        $Members.Add($Property, [Int16]"")
+
+  process {
+    $Properties = $Schema.properties | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name;
+    $Members = @{};
+    foreach ($Property in $Properties) {
+      switch ($Schema.properties.$Property.type) {
+        'object' {
+          $Members.Add($Property, (New-Object -TypeName psobject -Property @{}))
+        }
+        'array' {
+          $Members.Add($Property, @())
+        }
+        'string' {
+          $Members.Add($Property, "")
+        }
+        'number' {
+          $Members.Add($Property, [Int16]"")
+        }
       }
     }
+    New-Object -TypeName psobject -Property $Members;
   }
-  New-Object -TypeName psobject -Property $Members;
 }
 function Get-Array {
   param (
